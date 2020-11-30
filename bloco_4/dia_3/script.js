326a36b51be14037dc19d903a6c4ef1419237911
Felipe Rocha <felipe.sr27@gmail.com>
2020-11-30 13:15:31 -0300

    Pratica Javascript: Modulo 4 dia 3

--- bloco_4/dia_3/script.js
+++ bloco_4/dia_3/script.js
@@ -1,9 +1,9 @@
 //1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n .
-let number = 7;
+let number = 5;
 let asterisk = '';
 if ( number > 1 ){
     for ( let indice = 1 ; indice <= number ; indice += 1 ){
-        asterisk = '*' + asterisk;
+        asterisk += '*';
         if ( indice === number){
             for ( let indice_2 = 0 ; indice_2 < number ; indice_2 += 1) {
                 console.log(asterisk);
@@ -15,11 +15,11 @@ if ( number > 1 ){
 }
 
 //2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
-number =  6;
+number =  5;
 asterisk = '';
 if ( number > 0){
     for ( let indice = 1 ; indice <= number ; indice += 1 ){
-        asterisk = '*' + asterisk;
+        asterisk += '*';
         console.log(asterisk);
     }
 }else{    
@@ -27,15 +27,15 @@ if ( number > 0){
 }
 
 //3- Agora inverta o lado do triângulo. Por exemplo:
-number =  6;
+number =  5;
 if ( number > 0){
     for ( let indice = 1 ; indice <= number ; indice += 1 ){
         asterisk = '';
         for (let indice_2 = 1 ;  indice_2 <= (number-indice) ; indice_2 += 1){
-            asterisk = ' ' + asterisk;
+            asterisk += ' ';
         }
         for (let indice_2 = 1 ;  indice_2 <= (indice) ; indice_2 += 1){
-            asterisk = asterisk + '*';
+            asterisk +=  '*';
         }
         console.log(asterisk);
     }
@@ -44,16 +44,16 @@ if ( number > 0){
 }
 
 //4- Depois, faça uma pirâmide com n asteriscos de base:
-number =  9;
+number =  5;
 if ( number > 0){
     for ( let indice = 0 ; indice <= number ; indice += 1 ){
         if ((indice % 2) != 0 ){
             asterisk = ''; 
             for ( let indice_2 = number ;  indice_2 > (indice) ; indice_2 -=  2 ){
-                asterisk = ' ' + asterisk;
+                asterisk += ' ';
             }
             for ( let indice_3 = 1 ;  indice_3 <= (indice) ; indice_3 += 1 ){
-                asterisk = asterisk + '*';
+                asterisk += '*';
             }
             console.log(asterisk);
         }
