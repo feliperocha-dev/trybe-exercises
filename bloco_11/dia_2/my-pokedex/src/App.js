c03ac489e83ddca598e23158ac71dfdb4c1b9d50
Felipe Rocha <felipe.sr27@gmail.com>
2021-02-12 11:32:25 -0300

    React-Components: Pokedex Exercise

--- bloco_11/dia_2/my-pokedex/src/App.js
+++ bloco_11/dia_2/my-pokedex/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
 import Pokedex from './Component(s)/Pokedex.js';
+import Header from './Component(s)/Header.js';
 
 function App() {
-  return (<Pokedex />);
+  return (
+    <div>
+      <Header  />
+      <Pokedex />
+    </div>
+  );
 }
 
 export default App;
