fad2e0e51566ec4f784fe16f59325841e23b0d12
Felipe Rocha <felipe.sr27@gmail.com>
2021-07-27 11:25:33 -0300

    Deploy exercises

--- bloco_29/dia_1/heroku-deploy/indexProcfile.js
+++ bloco_29/dia_1/heroku-deploy/indexProcfile.js
@@ -8,7 +8,7 @@ app.get('/', (_req, res) => {
   res.status(200).send(`
     <body>
       <h1>Rodando na porta ${PORT}</h4>
-      <p>Variavel de ambiente: ${process.env.TESTE}</p>
+      <p>Ambiente: ${process.env.MESSAGE}</p>
     </body>  
   `);
 });
